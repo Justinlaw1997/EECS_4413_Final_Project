708b44de1a5fb42eed7e9d70a3539f6a97419dc5
Justinlaw1997 <65581948+Justinlaw1997@users.noreply.github.com>
2023-08-09 17:57:11 -0400

    Preliminary Files for Catalog

---
+++ src/main/java/dao/CatalogDAO.java
@@ -0,0 +1,27 @@
+package dao;
+
+import java.util.List;
+
+import model.Brand;
+import model.Category;
+import model.Item;
+
+public interface CatalogDAO {
+
+	public List<Item> findAllItems();
+	
+	public List<Item> SortItemsByAscendingPrice();
+	
+	public List<Item> SortItemsByDescendingPrice();
+	
+	public List<Item> SortItemsByAlphabeticalOrder();
+	
+	public List<Item> findItemsByCategory(String category);
+	
+	public List<Item> findItemsByBrand(String brand);
+
+	public List<Category> findAllCategories();
+
+	public List<Brand> findAllBrands();
+
+}
