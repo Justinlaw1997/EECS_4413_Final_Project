3cdaf0bc2e0fbbd73d935c147b235ea44856ab2e
lradda <leahradda@gmail.com>
2023-08-11 22:19:30 -0400

    userdao files and database update

--- src/main/java/controller/CatalogServlet.java
+++ src/main/java/controller/CatalogServlet.java
@@ -2,7 +2,6 @@ package controller;
 
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -12,9 +11,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import dao.ItemDAOImpl;
+import dao.UserDAOImpl;
+import model.Address;
 import model.Brand;
 import model.Category;
 import model.Item;
+import model.User;
 
 /**
  * Servlet implementation class CatalogServlet
@@ -40,48 +42,84 @@ public class CatalogServlet extends HttpServlet {
 		
 		ItemDAOImpl itemList = new ItemDAOImpl();
 		
-		// TESTING DAO IMPLEMENTATION (TO BE REMOVED)
+		// TESTING ITEM DAO IMPLEMENTATION (TO BE REMOVED)
 		List<Item> items1 = itemList.findAllItems();
-		System.out.println("-------------------");
+		System.out.println("-------------------FIND ALL ITEMS");
 		for(Item item: items1) {
 			System.out.println(item.toString());
 		}
 		List<Item> items2 = itemList.SortItemsByAscendingPrice();
-		System.out.println("-------------------");
+		System.out.println("-------------------SORT PRICE ASC");
 		for(Item item: items2) {
 			System.out.println(item.getPrice());
 		}
 		List<Item> items3 = itemList.SortItemsByDescendingPrice();
-		System.out.println("-------------------");
+		System.out.println("-------------------SORT PRICE DESC");
 		for(Item item: items3) {
 			System.out.println(item.getPrice());
 		}
 		List<Item> items4 = itemList.SortItemsByAlphabeticalOrder();
-		System.out.println("-------------------");
+		System.out.println("-------------------SORT ALPH");
 		for(Item item: items4) {
 			System.out.println(item.getName());
 		}
 		List<Item> items5 = itemList.findItemsByCategory("video game");
-		System.out.println("-------------------");
+		System.out.println("-------------------FIND BY CATEGORY");
 		for(Item item: items5) {
 			System.out.println(item.getName());
 		}
 		List<Item> items6 = itemList.findItemsByBrand("Dell");
-		System.out.println("-------------------");
+		System.out.println("-------------------FIND BY BRAND");
 		for(Item item: items6) {
 			System.out.println(item.getName());
 		}
 		List<Category> categories = itemList.findAllCategories();
-		System.out.println("-------------------");
+		System.out.println("-------------------FIND ALL CATEGORIES");
 		for(Category category: categories) {
 			System.out.println(category.toString());
 		}
 		List<Brand> brands = itemList.findAllBrands();
-		System.out.println("-------------------");
+		System.out.println("-------------------FIND ALL BRANDS");
 		for(Brand brand: brands) {
 			System.out.println(brand.toString());
 		}
 		
+		// TESTING USER DAO IMPLEMENTATION (TO BE REMOVED)
+		UserDAOImpl userList = new UserDAOImpl();
+		List<User> users1 = userList.findAllUsers();
+		System.out.println("-------------------FIND ALL USERS");
+		for(User user: users1) {
+			System.out.println(user.toString());
+		}
+		List<User> users2 = userList.findAllCustomers();
+		System.out.println("-------------------FIND CUSTOMERS");
+		for(User user: users2) {
+			System.out.println(user.toString());
+		}
+		List<User> users3 = userList.findAllAdmins();
+		System.out.println("-------------------FIND ADMINS");
+		for(User user: users3) {
+			System.out.println(user.toString());
+		}
+		Address address = new Address(99, "street", "province", "country", "code", "555");
+		User newUser = new User(99, "John", "Doe", address, 0, "test@email.com", "password");
+		userList.registerUser(newUser);
+		users1 = userList.findAllUsers();
+		System.out.println("-------------------ADD USER");
+		for(User user: users1) {
+			System.out.println(user.toString());
+		}			
+		userList.removeUser(newUser);
+		users1 = userList.findAllUsers();
+		System.out.println("-------------------REMOVE USER");
+		for(User user: users1) {
+			System.out.println(user.toString());
+		}	
+		System.out.println("-------------------SIGN IN RESULTS");
+		System.out.println(userList.signIn("'fakeuser'", "iamironman"));
+		System.out.println(userList.signIn("'tonystark@yahoo.ca'", "fakepass"));
+		System.out.println(userList.signIn("'dk@yahoo.ca'", "bananas4ever"));
+		System.out.println(userList.signIn("'bugs@gmail.com'", "whatsupdoc"));
 	}
 
 	/**
---
+++ src/main/java/dao/UserDAOImpl.java
@@ -0,0 +1,197 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Address;
+import model.User;
+
+public class UserDAOImpl implements UserDAO {
+	
+	static {
+		try {
+			Class.forName("org.sqlite.JDBC");
+		} catch (ClassNotFoundException ex) {}
+	}
+
+	private Connection getConnection() throws SQLException {
+		return DriverManager.getConnection("jdbc:sqlite:C:\\Users\\Leah\\Downloads\\teamJIL.db");
+	}
+	
+	private void closeConnection(Connection connection) {
+		if (connection == null) {
+			return;
+		}
+		
+		try {
+			connection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public List<User> findAllUsers() {
+		List<User> result = new ArrayList<User>();		
+		String query = "SELECT * FROM User INNER JOIN Address ON User.addressID = Address.id;";
+		queryUsers(query, result);
+		return result;
+	}
+
+	@Override
+	public List<User> findAllCustomers() {
+		List<User> result = new ArrayList<User>();		
+		String query = "SELECT * FROM User INNER JOIN Address ON User.addressID = Address.id WHERE isAdmin = 0;";
+		queryUsers(query, result);
+		return result;
+	}
+
+	@Override
+	public List<User> findAllAdmins() {
+		List<User> result = new ArrayList<User>();		
+		String query = "SELECT * FROM User INNER JOIN Address ON User.addressID = Address.id WHERE isAdmin = 1;";
+		queryUsers(query, result);
+		return result;
+	}
+	
+	@Override
+	public String signIn(String email, String password) {
+		String result = "";
+		Connection connection = null;
+		try {
+			connection = getConnection();
+			Statement statement = connection.createStatement();
+			ResultSet resultSet = statement.executeQuery("SELECT * FROM User WHERE email = " + email + ";");
+	
+			// Check if user exists
+			if (resultSet.next() == false) {
+				result = "user does not exist";
+			}						
+			
+			// Check if password is correct
+
+			else if (!resultSet.getString("password").equals(password)) {
+				result = "incorrect password";
+			}
+			
+			// Check if regular user or admin			
+			else {
+				result = (resultSet.getInt("isAdmin") == 0) ? "customer" : "admin";
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			closeConnection(connection);
+		}
+		
+		return result;
+	}
+	
+	@Override
+	public void registerUser(User user) {	
+		int userId = user.getId();
+		String firstName = user.getFirstName();
+		String lastName = user.getLastName();
+		int isAdmin = user.getIsAdmin();
+		String email = user.getEmail();
+		String password = user.getPassword();
+		
+		int addressId = user.getAddress().getId();
+		String streetAddress = user.getAddress().getStreetAddress();
+		String province = user.getAddress().getProvince();
+		String country = user.getAddress().getCountry();
+		String postalCode = user.getAddress().getPostalCode();
+		String phone = user.getAddress().getPhone();
+				
+		Connection connection = null;
+		try {
+			connection = getConnection();
+
+			String addressSQL = "INSERT into Address VALUES (?, ?, ?, ?, ?, ?);";
+			PreparedStatement addressStatement = connection.prepareStatement(addressSQL);
+			addressStatement.setInt(1, addressId);
+			addressStatement.setString(2, streetAddress);
+			addressStatement.setString(3, province);
+			addressStatement.setString(4, country);
+			addressStatement.setString(5, postalCode);
+			addressStatement.setString(6, phone);
+			addressStatement.execute();
+
+			String userSQL = "INSERT into User VALUES (?, ?, ?, ?, ?, ?, ?);";
+			PreparedStatement userStatement = connection.prepareStatement(userSQL);
+			userStatement.setInt(1, userId);
+			userStatement.setString(2, firstName);
+			userStatement.setString(3, lastName);
+			userStatement.setInt(4, addressId);
+			userStatement.setInt(5, isAdmin);
+			userStatement.setString(6, email);
+			userStatement.setString(7, password);
+			userStatement.execute();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			closeConnection(connection);
+		}
+	}
+	
+	@Override
+	public void removeUser(User user) {
+		int userId = user.getId();
+		int addressId = user.getAddress().getId();
+		
+		Connection connection = null;
+		try {
+			connection = getConnection();
+			Statement statement = connection.createStatement();
+			statement.executeUpdate("DELETE FROM User WHERE id = " + userId + ";");
+			statement.executeUpdate("DELETE FROM Address WHERE id = " + addressId + ";");
+				
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			closeConnection(connection);
+		}
+	}
+
+	private void queryUsers(String query, List<User> result) {	
+		Connection connection = null;
+		try {
+			connection = getConnection();
+			Statement statement = connection.createStatement();
+			ResultSet resultSet = statement.executeQuery(query);
+			
+			while(resultSet.next()) {	
+				int addressId = resultSet.getInt("addressId");
+				String streetAddress = resultSet.getString("streetAddress");
+				String province = resultSet.getString("province");
+				String country = resultSet.getString("country");
+				String postalCode = resultSet.getString("postalCode");
+				String phone = resultSet.getString("phone");
+				Address address = new Address(addressId, streetAddress, province, country, postalCode, phone);
+				
+				int userId = resultSet.getInt("id");
+				String firstName = resultSet.getString("firstName");
+				String lastName = resultSet.getString("lastName");
+				int isAdmin = resultSet.getInt("isAdmin");
+				String email = resultSet.getString("email");
+				String password = resultSet.getString("password");
+				
+				User user = new User(userId, firstName, lastName, address, isAdmin, email, password);				
+				result.add(user);
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			closeConnection(connection);
+		}
+	}
+}
