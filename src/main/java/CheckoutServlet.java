2d69402108ce3396bf6234f8bce8f776bb971616
Justinlaw1997 <65581948+Justinlaw1997@users.noreply.github.com>
2023-08-25 15:27:10 -0400

    Implemented base checkout

---
+++ src/main/java/CheckoutServlet.java
@@ -0,0 +1,60 @@
+
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.Address;
+import model.User;
+
+/**
+ * Servlet implementation class CheckoutServlet
+ */
+@WebServlet("/CheckoutServlet")
+public class CheckoutServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CheckoutServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	    HttpSession session = request.getSession();
+	    
+	    // Manage finding the session user
+	    // If session user does not exist, send them to the sign in/sign up page
+	    // Else
+	    // User user = session.getAttribute("user");
+	    
+	    Address address = new Address(101, "5 Random Street", "ON", "Country", "M1Z6H7", "4167877887");
+	    User user = new User(101, "Justin", "Law", address, 0, "justin@gmail.com", "password");
+	    session.setAttribute("user", user);
+		
+		RequestDispatcher rd;
+		rd = request.getRequestDispatcher("jsp/CheckoutView.jsp");
+		rd.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
